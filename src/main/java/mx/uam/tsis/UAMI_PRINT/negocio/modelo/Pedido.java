7859fed56f3436c5cd32fed6ecd5fdc9ee3c12eb
Helljav <miromancequimico_004@hotmail.com>
2020-07-10 22:50:01 -0500

    bueno

--- src/main/java/mx/uam/tsis/UAMI_PRINT/negocio/modelo/Pedido.java
+++ src/main/java/mx/uam/tsis/UAMI_PRINT/negocio/modelo/Pedido.java
@@ -1,21 +1,38 @@
 package mx.uam.tsis.UAMI_PRINT.negocio.modelo;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
 
 import org.springframework.web.multipart.MultipartFile;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-/*@NoArgsConstructor
+@NoArgsConstructor
 @AllArgsConstructor//Te genera los constructores gracias a la dependencia de Lombok
 @Builder 
 @Data // Permite la creacion de geters y seters con la dependencia lombok
 @Entity
 public class Pedido {
+	@Id
+	@GeneratedValue // Autogenera un ID unico
+	private Integer idPedido;
+	
+	@NotBlank
 	private String descripcionImpresion;
+	
+	@NotBlank
     private String rutaArchivo;
+	
+	@NotBlank
     private String tipoImpresion;
+	
+	@NotBlank
     private String metodoPago;
+	
+	
+	
 	@Override
 	public String toString() {
 		return "Pedido [descripcionImpresion=" + descripcionImpresion + ", rutaArchivo=" + rutaArchivo
@@ -24,4 +41,3 @@ public class Pedido {
     
     
 }
-*/
