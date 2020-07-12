79057aef33b8aa9f4fb0bae51cdc5eaadc74ad0d
JoseLSalgado <salgadojoseluis86@gmail.com>
2020-07-12 11:45:54 -0500

    Pruebas de manipulacion Archivo en PedidoController

--- src/main/java/mx/uam/tsis/UAMI_PRINT/servicio/PedidoController.java
+++ src/main/java/mx/uam/tsis/UAMI_PRINT/servicio/PedidoController.java
@@ -7,19 +7,28 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import javax.servlet.ServletException;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import lombok.extern.slf4j.Slf4j;
 import mx.uam.tsis.UAMI_PRINT.negocio.modelo.Pedido;
 /*Controlador tipo rest de capa de servicio*/
@@ -27,11 +36,11 @@ import mx.uam.tsis.UAMI_PRINT.negocio.modelo.Pedido;
 @Slf4j
 public class PedidoController {
 	
-	private Pedido pedido;
+	private Pedido pedido = new Pedido();
 	
 	
 	//Folder donde se guardaran los documentos
-	private static final String UPLOAD_DIR = "C:\\pruebas";
+	private static final String UPLOAD_DIR = "C://pruebas";
 	
 	
 	@PostMapping(path = "/cargar-archivo")
@@ -40,13 +49,12 @@ public class PedidoController {
 			@RequestParam("descripcion") String descripcionImpresion,
 			@RequestParam("archivo") MultipartFile archivo,
 			@RequestParam("tipo") String tipoImpresion,
-			@RequestParam("descripcion") String metodoPago
+			@RequestParam("pago") String metodoPago
 			
 			) throws IOException, ServletException {
 		
 		// filename completo del archivo
         String filename = UPLOAD_DIR + File.separator + archivo.getOriginalFilename();
-        
         
         pedido.setDescripcionImpresion(descripcionImpresion);
 		pedido.setMetodoPago(metodoPago);
@@ -71,7 +79,50 @@ public class PedidoController {
         
         log.info(pedido.toString());
         return new ResponseEntity(HttpStatus.ACCEPTED);
+	}
+	
+	
+	
+	/******************  METODOS ALTERNATIVOS ****************************************/
+	@PostMapping(path = "/descargar-archivo")
+	@ResponseBody
+	public ResponseEntity <?>descarga(
+			@RequestParam("descripcion") String descripcionImpresion,
+			@RequestParam("archivo") MultipartFile archivo,
+			@RequestParam("tipo") String tipoImpresion,
+			@RequestParam("pago") String metodoPago
+			
+			) throws IOException, ServletException {
+		
+		// filename completo del archivo
+        String filename = UPLOAD_DIR + "//" + archivo.getOriginalFilename();
+        log.info("Ruta de archivo almacenado "+filename);
+        
+        pedido.setDescripcionImpresion(descripcionImpresion);
+		pedido.setMetodoPago(metodoPago);
+		pedido.setTipoImpresion(tipoImpresion);
+		pedido.setRutaArchivo(filename);
+        
+	     // error si el archivo no se subio, viene vacio ó si el archivo ya existe en la carpeta        
+        if(archivo.isEmpty() || new File(filename).exists()) {
+ 			return new ResponseEntity(HttpStatus.BAD_REQUEST);
+ 		}
         
         
+		// Creacion de archivo en la carpeta
+        File fileSaveDir = new File(UPLOAD_DIR);
+        if (!fileSaveDir.exists()) { // crea el directorio si no existe
+            fileSaveDir.mkdirs();
+        }         
+		        
+        FileOutputStream salidaArchivo = new FileOutputStream( filename );
+        salidaArchivo.write( archivo.getBytes() );
+        salidaArchivo.close();
+        
+        log.info(pedido.toString());
+        //return new ResponseEntity(HttpStatus.ACCEPTED);
+        
+        return ResponseEntity.status(HttpStatus.CREATED).body(pedido);
+       
 	}
 }
